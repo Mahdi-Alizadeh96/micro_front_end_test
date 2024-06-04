aa66326de0a0e3bdfe746ee16bbb9e3afd92bf6c
mahdi.alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-06-04 13:18:30 +0330

    upgrade webpack versions

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -2,20 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
-  mode: 'development',
-  devServer: {
-    port: 8081,
+    mode: 'development',
+    devServer: {
+        port: 8081,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'products',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './ProductsIndex': './src/index',
-      },
+        name: 'products',
+        filename: 'remoteEntry.js',
+        exposes: {
+            './ProductsIndex': './src/index',
+        },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+        template: './public/index.html',
     }),
   ],
 };
