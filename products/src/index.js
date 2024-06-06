10aff29e84d630a47096f7ba1fb786d35574d17d
mahdi.alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-06-06 22:36:01 +0330

    refactoring products

--- products/src/index.js
+++ products/src/index.js
@@ -1,12 +1,38 @@
-import('faker').then(faker => {
+const e = import('faker').then(faker => {
+    
+    function mount(el) {
 
-    let products = '';
+        let products = '';
+        
+        for (let i = 0; i < 5; i++) {
     
-    for (let i = 0; i < 5; i++) {
-      const name = faker.commerce.productName();
-      products += `<div>${name}</div>`;
+          const name = faker.commerce.productName();
+          products += `<div>${name}</div>`;
+    
+        };
+        
+        el.innerHTML = products;
+        
     }
-    
-    document.querySelector('#dev-products').innerHTML = products;
+
+    if (process.env.NODE_ENV === 'development') {
+
+        const element = document.querySelector('#dev-products');
+
+        if (element) {
+
+            mount(element);
+
+        } else {
+
+            throw new Error("dev-products element not found")
+
+        }
+
+    };
+
+    return mount;
 
 });
+
+export { e }
