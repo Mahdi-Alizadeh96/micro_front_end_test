48b6ba878616f272c775a14df5f1bb9db7b492f6
mahdi.alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-06-07 15:38:03 +0330

    initial setup for framework based project

--- container/src/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import { mount } from 'products/ProductsIndex';
-import { mount as cartMount } from 'cart/cartItems';
-
-console.log('Container!');
-
-await cartMount(document.querySelector("#container-cart"))
-
-mount(document.querySelector("#container-products"));
