f620327bdd0ace7f7a3e22b408aa3c9d3dcbe50f
mahdi.alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-06-07 20:52:19 +0330

    add webpack configs for common and dev enviroments

---
+++ marketing/config/webpack.dev.js
@@ -0,0 +1,20 @@
+import { merge } from 'webpack-merge';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import commonConfig from './webpack.common';
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+export default merge(commonConfig, devConfig);
