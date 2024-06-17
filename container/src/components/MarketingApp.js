0eb36a5763abb14544bb03848c06d0f9b635c6e2
mahdi.alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-06-17 15:23:07 +0330

    render marketing app in container

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,16 @@
+import { mount } from 'marketing/MarketingApp';
+import React, { useEffect, useRef } from 'react';
+
+export default function MarketingApp() {
+
+    const ref = useRef(null);
+
+    useEffect(() => {
+
+        mount(ref.current);
+
+    });
+
+    return <div ref={ref} />;
+    
+}
