c8abf65cdfd2976ebecb81ed3f6f2bccb263d0cd
mahdi.alizadeh <mahdi.alizadeh.sp@gmail.com>
2024-06-25 13:51:24 +0330

    Integration Dashboard with the Container

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -9,14 +9,17 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/',
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`,
-            },
-            shared: packageJson.dependencies,
+        name: 'container',
+        remotes: {
+            marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+            auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+            dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
+        },
+        shared: packageJson.dependencies,
         }),
     ],
 };
